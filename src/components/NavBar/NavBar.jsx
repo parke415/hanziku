d5280f1e635a693d715e77ed2cae4d2b095eff96
Jim Clark <jim.clark@generalassemb.ly>
2021-01-20 09:52:27 -0800

    Finish sign up

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,11 +1,13 @@
 import { NavLink } from 'react-router-dom';
 
-export default function NavBar() {
+export default function NavBar({ user }) {
   return (
     <nav>
       <NavLink exact activeStyle={{backgroundColor: 'yellow'}} to="/orders">Order History</NavLink>
       &nbsp; | &nbsp;
       <NavLink exact activeStyle={{backgroundColor: 'yellow'}} to="/orders/new">New Order</NavLink>
+      &nbsp; | &nbsp;
+      <span>Welcome, {user.name}</span>
     </nav>
   );
 }
--- src/utilities/users-service.js
+++ src/utilities/users-service.js
@@ -3,8 +3,30 @@ import * as usersAPI from './users-api';
 export async function signUp(userData) {
   try {
     const token = await usersAPI.signUp(userData);
-    return token;
+    // Persist the token
+    localStorage.setItem('token', token);
+    return getUser();
   } catch {
     throw new Error('Invalid Sign Up');
   }
+}
+
+export function getToken() {
+  // getItem returns null if there's no string
+  const token = localStorage.getItem('token');
+  if (!token) return null;
+  // Check if expired, remove if it is
+  const payload = JSON.parse(atob(token.split('.')[1]));
+  // A JWT's exp is expressed in seconds, not milliseconds, so convert
+  if (payload.exp < Date.now() / 1000) {
+    localStorage.removeItem('token');
+    return null;
+  }
+  return token;
+}
+
+export function getUser() {
+  const token = getToken();
+  // If there's a token, return the user in the payload, otherwise return null
+  return token ? JSON.parse(atob(token.split('.')[1])).user : null;
 }
