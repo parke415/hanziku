1808fdba3d1145517a5bf7009695136fed7df823
parke415 <parke415@gmail.com>
2021-01-28 13:39:50 -0800

    adds server CRUD for main model

---
+++ controllers/api/characters.js
@@ -0,0 +1,80 @@
+const Character = require('../../models/character');
+
+module.exports = {
+  index,
+  show,
+  new: newChar,
+  create,
+  edit,
+  update,
+  delete: deleteChar,
+  learnToggle,
+};
+
+function index(req, res) {
+  Character.find({user: req.user._id}, (err, characters) => {
+    let learnedChars = 0;
+    characters.forEach(character => {if (character.learned) learnedChars++});
+    res.render(`characters/index`, {title: 'Character List', characters, learnedChars});
+  });
+}
+
+function show(req, res) {
+  Character.findById(req.params.id, (err, character) => res.render(`characters/show`, {title: 'Character Details', character, user: req.user._id}));
+}
+
+function newChar(req, res) {
+  res.render(`characters/new`, {title: 'New Character'});
+}
+
+function create(req, res) {
+  req.body.user = req.user._id;
+  const character = new Character(req.body);
+  Character.find({user: req.user._id, glyph: character.glyph}, (err, duplicate) => {
+    if (err || duplicate.length) return res.redirect(`/characters/new`);
+    character.save(err => {
+      if (err) return res.redirect(`/characters/new`);
+      res.redirect(`/characters/${character._id}`);
+    });
+  });
+}
+
+function edit(req, res) {
+  Character.findById(req.params.id, (err, character) => {
+    if (!character.user.equals(req.user._id)) return res.redirect(`/characters/${character._id}`);
+    res.render(`characters/edit`, {title: 'Edit Character', character});
+  });
+}
+
+function update(req, res) {
+  Character.findById(req.params.id, (err, character) => {
+    if (!character.user.equals(req.user._id)) return res.redirect(`/characters/${character._id}`);
+    Object.assign(character, req.body);
+    character.save(err => {
+      if (err) return res.redirect(`/characters/new`);
+      res.redirect(`/characters/${character._id}`);
+    });
+  });
+}
+
+function deleteChar(req, res) {
+  Character.findById(req.params.id, (err, character) => {
+    if (!character.user.equals(req.user._id)) return res.redirect(`/characters/${character._id}`);
+    character.remove();
+    res.redirect(`/characters`);
+  });
+}
+
+function learnToggle(req, res) {
+  Character.findById(req.params.id, (err, character) => {
+    if (!character.user.equals(req.user._id)) return res.redirect(`/characters/${character._id}`);
+    character.learned = !character.learned;
+    character.save(err => {
+      if (character.learned) {
+        res.redirect(`/characters`);
+      } else {
+        res.redirect(`/characters/${character._id}`);
+      }
+    });
+  });
+}
---
+++ routes/api/characters.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const charactersCtrl = require('../../controllers/api/characters');
+const ensureLoggedIn = require('../../config/ensureLoggedIn');
+
+router.get('/', charactersCtrl.index);
+router.get('/new', ensureLoggedIn, charactersCtrl.new);
+router.get('/:id', charactersCtrl.show);
+router.post('/', ensureLoggedIn, charactersCtrl.create);
+router.get('/:id/edit', ensureLoggedIn, charactersCtrl.edit);
+router.put('/:id', ensureLoggedIn, charactersCtrl.update);
+router.delete('/:id', ensureLoggedIn, charactersCtrl.delete);
+router.put('/:id/learnToggle', ensureLoggedIn, charactersCtrl.learnToggle);
+
+module.exports = router;
